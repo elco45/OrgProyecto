2303bea5f266e4f11aed954cba5735b8bb0f0ae5
elco45 <dennischeong2003@tahoo.es>
2015-09-12 11:59:53 -0600

    Listar

--- main.cpp
+++ main.cpp
@@ -414,3 +414,100 @@ void WLlamadaBin(){
 	}
 	inFile.close();
 }
+
+//Listar
+void L_CiudadBin(){
+	ifstream inFile("ciudad.bin",ios::binary);
+	inFile.seekg(0);
+	int avail;
+	int cantRegistros;
+	bool flag;
+	inFile.read((char*)&avail, sizeof(int));
+	inFile.read((char*)&cantRegistros, sizeof(int));
+	inFile.read((char*)&flag, sizeof(bool));
+	cout<<avail<<", "<<cantRegistros<<", "<<flag<<endl;
+	int cont=0;
+	while(cont<cantRegistros){
+		char IdCiudad[5];
+		char NombreCiudad[40];
+		inFile.read((char*)IdCiudad, sizeof(IdCiudad));
+		inFile.read((char*)NombreCiudad, sizeof(NombreCiudad));
+		if (IdCiudad[0]!='*'){
+			cout<<IdCiudad<<","<<NombreCiudad<<endl;
+		}
+		cont++;
+	}
+	inFile.close();
+}
+void L_ClienteBin(){
+	ifstream inFile("cliente.bin",ios::binary);
+	inFile.seekg(0);
+	int avail;
+	int cantRegistros;
+	bool flag;
+	inFile.read((char*)&avail, sizeof(int));
+	inFile.read((char*)&cantRegistros, sizeof(int));
+	inFile.read((char*)&flag, sizeof(bool));
+	cout<<avail<<", "<<cantRegistros<<", "<<flag<<endl;
+	int cont=0;
+	while(cont<cantRegistros){
+		char IdCliente[15];
+		char NombreCliente[40];
+		char Genero[2];
+		char IdCiudad[5];
+		inFile.read((char*)IdCliente, sizeof(IdCliente));
+		inFile.read((char*)NombreCliente, sizeof(NombreCliente));
+		inFile.read((char*)Genero, sizeof(Genero));
+		inFile.read((char*)IdCiudad, sizeof(IdCiudad));
+		cout <<IdCliente << "," << NombreCliente << "," << Genero << "," << IdCiudad <<endl;
+		cont++;
+	}
+	inFile.close();
+}
+void L_LineaBin(){
+	ifstream inFile("linea.bin",ios::binary);
+	inFile.seekg(0);
+	int avail;
+	int cantRegistros;
+	bool flag;
+	inFile.read((char*)&avail, sizeof(int));
+	inFile.read((char*)&cantRegistros, sizeof(int));
+	inFile.read((char*)&flag, sizeof(bool));
+	cout<<avail<<", "<<cantRegistros<<", "<<flag<<endl;
+	int cont=0;
+	while(cont<cantRegistros){
+		char Numero[9];
+		char Id[14];
+		inFile.read((char*)Id, sizeof(Id));
+		inFile.read((char*)Numero, sizeof(Numero));
+		cout << Id << "," << Numero << endl;
+		cont++;
+	}
+	inFile.close();
+}
+void L_LlamadaBin(){
+	ifstream inFile("llamada.bin",ios::binary);
+	inFile.seekg(0);
+	int avail;
+	int cantRegistros;
+	bool flag;
+	inFile.read((char*)&avail, sizeof(int));
+	inFile.read((char*)&cantRegistros, sizeof(int));
+	inFile.read((char*)&flag, sizeof(bool));
+	cout<<avail<<", "<<cantRegistros<<", "<<flag<<endl;
+	int cont=0;
+	while(cont<cantRegistros){
+		char Numero[9];
+		char inic[20];
+		char fin[20];
+		char IdCiudad[4];
+		inFile.read((char*)&Numero, sizeof(Numero));
+		inFile.read((char*)&inic, sizeof(inic));
+		inFile.read((char*)&fin, sizeof(fin));
+		inFile.read((char*)&IdCiudad, sizeof(IdCiudad));
+		cout <<Numero << "," << inic << "," << fin << "," << IdCiudad <<endl;
+		cont++;
+	}
+	inFile.close();
+}
+
