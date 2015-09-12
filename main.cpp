0e3024e8cbd8960af0b1b4d8804201b465778f0e
elco45 <dennischeong2003@tahoo.es>
2015-09-12 12:01:43 -0600

    Herramientas

--- main.cpp
+++ main.cpp
@@ -511,3 +511,83 @@ void L_LlamadaBin(){
 	inFile.close();
 }
 
+//Herramientas de busqueda	
+int PosNuevoBinarySearch(vector<Index> v,unsigned long key){
+    int izquierda = 0;
+    int derecha = v.size() - 1;
+    int medio;
+    bool fin = false;
+    while(izquierda<derecha){   	
+    	medio = (izquierda + derecha)/2;
+    	if(key > v.at(medio).llave){
+    		if(medio != v.size()-1){
+    			if(key < v.at(medio+1).llave){
+    				return medio+1;
+    			}else{
+    				if(fin && medio == v.size()-1){
+    					return -1;
+    				}
+    				izquierda = medio;
+    				fin = true;
+    			}
+    		}else{
+    			if(key < v.at(medio).llave){
+    				return medio;
+    			}else{
+    				return -1;
+    			}
+    		}
+    	}else{
+    		if(medio != 0){
+    			if(key > v.at(medio-1).llave){
+    				return medio;
+    			}else{
+    				derecha = medio;
+    			}
+    		}else{
+    			if(key < v.at(medio).llave){
+    				return medio;
+    			}else{
+    				return medio+1;
+    			}
+    		}
+    	}
+    }
+    return -1;
+}
+int PosBinarySearch(vector<Index> v,unsigned long key){
+	int tama = v.size();
+	int inferior,superior,medio;
+	inferior = 0;
+	superior = tama;
+	while(inferior <= superior){
+		medio = (inferior+superior)/2;
+		if(v.at(medio).llave==key){
+			return medio;	
+		}
+		if(v.at(medio).llave>key){
+			superior = medio;
+			medio =(inferior+superior)/2;
+		}
+		if (v.at(medio).llave<key){
+			inferior = medio;
+			medio =(inferior+superior)/2;
+		}
+	}
+}
+bool binarySearch(vector<Index> v, unsigned long key, int start,int end){
+    if(end < start)
+        return false;
+    else{
+        int mid = (start + end)/2;
+        if(v.at(mid).llave > key){
+            return binarySearch(v, key, start, mid-1);
+        }
+        else if (v.at(mid).llave < key){
+            return binarySearch(v, key, mid+1, end);
+        }
+        else{
+            return true;
+        }
+    }
+}
